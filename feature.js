7704d7e22511d86e9faa8ec2a0c7cd0b567ecec4
Saud <saudkhan2454@gmail.com>
2023-01-11 15:13:05 +0500

    play with friend & computer

--- feature.js
+++ feature.js
@@ -2,92 +2,99 @@ function checkWin() {
     var inputs = [
         document.getElementById("b1"), document.getElementById("b2"), document.getElementById("b3"),
         document.getElementById("b4"), document.getElementById("b5"), document.getElementById("b6"),
-        document.getElementById("b7"), document.getElementById("b8"), document.getElementById("b9")
+        document.getElementById("b7"), document.getElementById("b8"), document.getElementById("b9"),
     ]
 
     for (var i = 0; i < inputs.length; i++) {
         inputs[i] = inputs[i].innerHTML
     }
-  
 
+    const symbols = ['x', '0']
+    for (let i = 0; i < 2; i++) {
+        if (checkWinForPlayer(symbols[i], inputs)) {
+            document.getElementById('print').innerHTML = "player " + symbols[i] + " won";
+            window.alert("player " + symbols[i] + " won")
+            const ev = new Event("win");
+            document.dispatchEvent(ev)
+            document.getElementById("b2").disabled = true;
+            document.getElementById("b3").disabled = true;
+            document.getElementById("b4").disabled = true;
+            document.getElementById("b6").disabled = true;
+            document.getElementById("b7").disabled = true;
+            document.getElementById("b8").disabled = true;
 
-
-    if ((inputs[0] != '') && (inputs[1] != '') && (inputs[2] != '') && (inputs[3] != '') && (inputs[4] != '') && (inputs[5] != '') && (inputs[6] != '') && (inputs[7] != '') && (inputs[8] != '0')) {
+            return
+        }
+    }
+    
+    if ((inputs[0] != '') && (inputs[1] != '') && (inputs[2] != '') && (inputs[3] != '') && (inputs[4] != '') && (inputs[5] != '') && (inputs[6] != '') && (inputs[7] != '') && (inputs[8] != '')) {
         document.getElementById('print').innerHTML = "Match Tie";
         window.alert('Match Tie');
     }
-    else {
-        checkWinForPlayer('x', inputs)
 
-        checkWinForPlayer('0', inputs)
-        if (flag == 1) {
-            document.getElementById('print').innerHTML = "player X Turn";
-        }
-        else {
-            document.getElementById('print').innerHTML = "player 0 Turn";
-        }
+    if (flag == 1) {
+        document.getElementById('print').innerHTML = "player X Turn";
+    }
+    else {
+        document.getElementById('print').innerHTML = "player 0 Turn";
     }
 }
 
-document.addEventListener("win",playAudio);
-function playAudio(){
+document.addEventListener("win", playAudio);
+function playAudio() {
     var x = document.getElementById("myAudio").play();
 }
+// function computerMove(id) {
+//     var emptyCells = [];
+//     var random;
+
+//     for (var i = 0; i < inputs.length; i++) {
+//       if (inputs[i] == '') {
+//         emptyCells.push(inputs[i]);
+//       }
+//     }
+
+//     inputs.forEach(function(inputs){
+//       if (inputs == '') {
+//         emptyCells.push(inputs);
+//       }
+//     });
+
+//     // computer marks a random EMPTY cell
+//     random = Math.ceil(Math.random() * emptyCells.length) - 1;
+//     emptyCells[random] = symbol;
+//     checkWinForPlayer();
+//     updateCell();
+//   }
+
 
 function checkWinForPlayer(symbol, inputs) {
     for (var i = 0; i < inputs.length; i += 3) {
         if ((inputs[i].toLowerCase() == symbol) &&
             (inputs[i + 1].toLowerCase() == symbol) &&
             (inputs[i + 2].toLowerCase() == symbol)) {
-            document.getElementById('print').innerHTML = "player " + symbol + "won";
-            window.alert('player' + symbol + 'won')
-            const ev= new Event("win");
-            document.dispatchEvent(ev)
+            return true;
         }
     }
     for (var i = 0; i < inputs.length / 3; i += 1) {
-        console.log(i)
         if ((inputs[i].toLowerCase() == symbol) &&
             (inputs[i + 3].toLowerCase() == symbol) &&
             (inputs[i + 6].toLowerCase() == symbol)) {
-            document.getElementById('print').innerHTML = "player " + symbol + "won";
-            window.alert('player' + symbol + 'won')
-            const ev= new Event("win");
-            document.dispatchEvent(ev)
+                return true
         }
-
-       
-        
     }
     if ((inputs[0].toLowerCase() == symbol) &&
         (inputs[4].toLowerCase() == symbol) &&
         (inputs[8].toLowerCase() == symbol)) {
-        document.getElementById('print').innerHTML = "Player " + symbol + " won";
-        document.getElementById("b2").disabled = true;
-        document.getElementById("b3").disabled = true;
-        document.getElementById("b4").disabled = true;
-        document.getElementById("b6").disabled = true;
-        document.getElementById("b7").disabled = true;
-        document.getElementById("b8").disabled = true;
-        window.alert('Player ' + symbol + ' won');
-        const ev= new Event("win");
-        document.dispatchEvent(ev)
-        
+        return true
     }
     else if ((inputs[2].toLowerCase() == symbol) &&
         (inputs[4].toLowerCase() == symbol) &&
         (inputs[6].toLowerCase() == symbol)) {
-        document.getElementById('print').innerHTML = "Player " + symbol + " won";
-        document.getElementById("b1").disabled = true;
-        document.getElementById("b2").disabled = true;
-        document.getElementById("b4").disabled = true;
-        document.getElementById("b6").disabled = true;
-        document.getElementById("b8").disabled = true;
-        document.getElementById("b9").disabled = true;
-        window.alert('Player ' + symbol + ' won');
-        const ev= new Event("win");
-        document.dispatchEvent(ev)
+        return true
     }
+
+    return false
 }
 
 function reset() {
@@ -104,9 +111,8 @@ function reset() {
 }
 flag = 1;
 function updateCell(id) {
-    if ( document.getElementById(id).innerHTML != '')
-    return
     if (flag == 1) {
+        console.log("player2")
         document.getElementById(id).innerHTML = "X";
         document.getElementById(id).disabled = true;
         flag = 0;
@@ -116,30 +122,51 @@ function updateCell(id) {
         document.getElementById(id).disabled = true;
         flag = 1;
     }
+
+    if (player2 == "") {
+        computerTurn(flag)
+        flag = 1
+    }
+}
+
+function computerTurn(flag) {
+    const freePositions = []
+    const positions = document.querySelectorAll('.board-button')
+
+    positions.forEach(position => {
+        if (!position.innerHTML) freePositions.push(position)
+    })
+
+    if (freePositions.length <= 0)
+        return
+
+    const randomMoveIndex = Math.ceil(Math.random() * freePositions.length -1);
+
+    freePositions[randomMoveIndex].innerHTML = flag === 0 ? '0' : 'X'
 }
 
 window.matchMedia('(prefers-color-scheme: dark)')
-      .addEventListener('change', ({ matches }) => {
-  if (matches) {
-    document.querySelector('html').classList.add('dark-mode')
-    console.log("change to dark mode!")
-  } else {
-    document.querySelector('html').classList.remove('dark-mode')
-    console.log("change to light mode!")
-  }
-})
+    .addEventListener('change', ({ matches }) => {
+        if (matches) {
+            document.querySelector('html').classList.add('dark-mode')
+            console.log("change to dark mode!")
+        } else {
+            document.querySelector('html').classList.remove('dark-mode')
+            console.log("change to light mode!")
+        }
+    })
 
 
 const urlparams = new URLSearchParams(window.location.search);
 
 
-const player1=urlparams.get('player1');
+const player1 = urlparams.get('player1');
 // player1=document.getElementById('player1-name').value;
 // document.getElementById("player1-name").innerHTML='player1';
-//  
+const Computer = urlparams.get('Computer')
 
-const player2=urlparams.get('player2');
+const player2 = urlparams.get('player2');
 // document.getElementById("player2-name").innerHTML='player2';
 
 
-console.log("player names:", player1, player2);
+console.log("player names:", player1, Computer, player2);
--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 
 function myfunction() {
     var p1=document.getElementById('b1').value;
-    var p2=document.getElementById('b2').value;
+var p2=document.getElementById('comp').value;
+    var p3=document.getElementById('b2').value;
   
-    window.open("http://127.0.0.1:5500/new-feature.html?player1=" + p1 + "&player2=" +p2)
+    window.open("http://127.0.0.1:5500/new-feature.html?player1=" + p1 +"&Computer="+ p2+ "&player2=" +p3)
 
     }
     
+
+    function userinput() {
+        // get the clock
+        var mydiv = document.getElementById('secondplayer');
+    
+        // get the current value of the clock's display property
+        var displaySetting = mydiv.style.display;
+    
+        // also get the clock button, so we can change what it says
+        var friend = document.getElementById('friend');
+    
+        // now toggle the clock and the button text, depending on current state
+        if (displaySetting == 'block') {
+          // clock is visible. hide it
+          mydiv.style.display = 'none';
+          // change button text
+          friend.innerHTML = 'Show content';
+        }
+        else {
+          // clock is hidden. show it
+          mydiv.style.display = 'block';
+          // change button text
+          friend.innerHTML = 'Enter player name';
+        }
+      }
